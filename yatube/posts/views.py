e7f47ca9080cea696e8cb1a72f0accc9c7ba02c1
Артём Жуков <zo.sahaal.13@gmail.com>
2022-01-10 11:42:46 +0300

    fix structure code.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
+SLICE = 10
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.select_related('group')[:SLICE]
+    title = 'Последние обновления на сайте'
     context = {
         'posts': posts,
+        'title': title,
     }
     return render(request, template, context)
 
@@ -14,7 +17,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).select_related('group')[:SLICE]
     context = {
         'group': group,
         'posts': posts,
